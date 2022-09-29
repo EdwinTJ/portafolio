44e349156910a5b7a798d4c9d10365a9c78f8fe7
EdwinTJ <edwin16x@gmail.com>
2022-09-29 14:22:08 -0600

    Add Folders, Dependencies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import "./App.css";
-import linkedin from "./Images/linkedin.svg";
-import gitHub from "./Images/github.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a
-          href="https://www.linkedin.com/in/edwin-silvestre/"
-          target="_blank"
-          rel="noopener"
-        >
-          <img src={linkedin} alt="linkedin" />
-          Linkedin Profile
-        </a>
-        <a href="https://github.com/EdwinTJ" target="_blank" rel="noopener">
-          <img src={gitHub} alt="linkedin" />
-          Linkedin Profile
-        </a>
-
         <p>Site Under Construction</p>
       </header>
     </div>
