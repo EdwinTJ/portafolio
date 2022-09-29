ca26f884093062b4a6738777a5202797f35eac46
EdwinTJ <edwin16x@gmail.com>
2022-09-29 17:20:17 -0600

    Projects page

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,7 +1,49 @@
 import React from "react";
-
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import SchoolIcon from "@mui/icons-material/School";
+import WorkIcon from "@mui/icons-material/Work";
 function Experience() {
-  return <div>Experience</div>;
+  return (
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2019-2021"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Utah State University — Logan, UT
+          </h3>
+          <p>2 years towards Computer Science Degree</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2022"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<SchoolIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Bridgerland Technical College,Logan, UT
+          </h3>
+          <p>Web and Mobile Development Certificate</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2019-2021"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+          icon={<WorkIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">Insurance Website</h3>
+          <p>Create the back end for a contact form</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 }
 
 export default Experience;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,39 @@
 import React from "react";
-
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
+import "../styles/Home.css";
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="about">
+        <h2>Hi, My Name is Edwin Silvestre</h2>
+        <div className="prompt">
+          <p>I'm a web develper</p>
+          <GitHubIcon />
+          <LinkedInIcon />
+          <EmailIcon />
+        </div>
+      </div>
+      <div className="skills">
+        <h1>Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2>Front-End</h2>
+            <span>ReactJS,HTML5,CSS,NPM,Bootstrap,MaterialUI</span>
+          </li>
+          <li className="item">
+            <h2>Back-End</h2>
+            <span>NodeJS,MySQL,Git</span>
+          </li>
+          <li className="item">
+            <h2>Languaes</h2>
+            <span>JavaScript,PHP,Python</span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
