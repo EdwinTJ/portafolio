c0a94137a6f3468447a9d189c4cc77880a3a84b1
EdwinTJ <edwin16x@gmail.com>
2022-09-29 14:30:00 -0600

    Create pages, styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Site Under Construction</p>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
