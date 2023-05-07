38393c0b81f30acd2d4e3c1f72ba0c3da8b1c7ff
EdwinTJ <edwin16x@gmail.com>
2023-05-07 13:44:44 -0600

    Add New project

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -10,6 +10,7 @@ export const ProjectList = [
     github: "https://github.com/EdwinTJ/menu",
     description: "Display of a menu",
     link: "https://fancy-queijadas-35ef1b.netlify.app/",
+    client: "Course",
   },
   {
     name: "Chat App",
@@ -18,22 +19,26 @@ export const ProjectList = [
     github: "https://github.com/EdwinTJ/chat-app-server",
     description: "Chat wiht someone in the same room",
     link: "https://chat-app-ui-production.up.railway.app/",
+    client: "Course",
   },
 
   {
-    name: "Mercancia Tijuana",
+    name: "Anime Shop Tijuana",
     image: MercanciaTijuana,
-    github: "https://github.com/EdwinTJ/bodega_website",
-    skills: "React,NodeJS,Express,MongoDB,Nelify,Railway",
+    github: "https://figuras-website.vercel.app/",
+    skills: "React,NodeJS,Express,MongoDB,Vercel,Railway",
     description:
-      "This website has the porpuse of display the products of a company. Also, it has an admin panel where the admin can add, edit and delete products.",
-    link: "https://stalwart-bavarois-0d8052.netlify.app/",
+      "This website has the porpuse of display the products of a company. Also, it has an admin panel where the admin can add, edit and delete products." +
+      " The backend was created with express and the frontend with React" +
+      " For the database I use MongoDB",
+    link: "https://figuras-website.vercel.app/",
+    client: "Shop In Tijuana",
   },
 
   {
     name: "Icon Twitter",
     image: IconTwitter,
-    github: "https://github.com/EdwinTJ/icon-twitter",
+    github: "https://icon-twitter-uf42.vercel.app/",
     skills: "NextJs,TypeScript,Zod,tRPC,Tailwindcss,Prisma,dayjs",
     description:
       "This website a replica of the twitter app but only accepts emojis. This project was created by using create-t3-app." +
@@ -42,5 +47,6 @@ export const ProjectList = [
       " I use typescript for the whole project and zod for the validation of the data." +
       " For the deployment I use Vercel, and planetScale for the database hosting",
     link: "https://icon-twitter-uf42-ipucn3gq4-edwintj.vercel.app/",
+    client: "Course",
   },
 ];
